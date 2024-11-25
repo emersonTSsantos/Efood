a8a366abb1f11bf6023dee638a06c19ad555a7ea
Emerson Teixeira <emerson@exemplo.com>
2024-11-25 18:28:14 -0300

    Criando a Modal para os pratos

--- src/components/PerfilRestaurante/index.tsx
+++ src/components/PerfilRestaurante/index.tsx
@@ -1,7 +1,9 @@
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import logo from '../../assets/images/logo.png'
 import Pizza from '../../assets/images/pizza.png'
+import Fechar from '../../assets/images/fechar.png'
 
 import {
   Header,
@@ -12,54 +14,97 @@ import {
   Container,
   PerfilRestaurante,
   NomeRestaurente,
-  AddButton,
+  AbrirModal,
   Card,
   CardContent,
   CardDescription,
   CardImage,
   CardTitle,
-  CardsGrid
+  CardsGrid,
+  ModalBackdrop,
+  ModalContent,
+  AddButton
 } from './styles'
 
-const Perfil = () => (
-  <div>
-    <Header>
-      <NavContainer>
-        <Link to="/">Restaurantes</Link>
-      </NavContainer>
-      <Logo src={logo} alt="Logo Efood" />
-      <CartInfo>
-        <span>0 </span>
-        produto(s) no carrinho
-      </CartInfo>
-    </Header>
-    <Hero>
-      <Container>
-        <PerfilRestaurante>Italiana</PerfilRestaurante>
-        <NomeRestaurente>La Dolce Vita Trattoria</NomeRestaurente>
-      </Container>
-    </Hero>
-    <section>
-      <Container>
-        <CardsGrid>
-          {[...Array(6)].map((_, index) => (
-            <Card key={index}>
-              <CardImage src={Pizza} alt="Pizza" />
-              <CardContent>
-                <CardTitle>Pizza Marguerita</CardTitle>
-                <CardDescription>
-                  A clássica Marguerita: molho de tomate suculento, mussarela
-                  derretida, manjericão fresco e um toque de azeite. Sabor e
-                  simplicidade!
-                </CardDescription>
-                <AddButton>Adicionar ao carrinho</AddButton>
-              </CardContent>
-            </Card>
-          ))}
-        </CardsGrid>
-      </Container>
-    </section>
-  </div>
-)
+const Perfil = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
+  return (
+    <div>
+      <Header>
+        <NavContainer>
+          <Link to="/">Restaurantes</Link>
+        </NavContainer>
+        <Logo src={logo} alt="Logo Efood" />
+        <CartInfo>
+          <span>0 </span>
+          produto(s) no carrinho
+        </CartInfo>
+      </Header>
+      <Hero>
+        <Container>
+          <PerfilRestaurante>Italiana</PerfilRestaurante>
+          <NomeRestaurente>La Dolce Vita Trattoria</NomeRestaurente>
+        </Container>
+      </Hero>
+      <section>
+        <Container>
+          <CardsGrid>
+            {[...Array(6)].map((_, index) => (
+              <Card key={index}>
+                <CardImage src={Pizza} alt="Pizza" />
+                <CardContent>
+                  <CardTitle>Pizza Marguerita</CardTitle>
+                  <CardDescription>
+                    A clássica Marguerita: molho de tomate suculento, mussarela
+                    derretida, manjericão fresco e um toque de azeite. Sabor e
+                    simplicidade!
+                  </CardDescription>
+                  <AbrirModal onClick={handleOpenModal}>
+                    Adicionar ao carrinho
+                  </AbrirModal>
+                </CardContent>
+              </Card>
+            ))}
+          </CardsGrid>
+        </Container>
+      </section>
+
+      {isModalOpen && (
+        <ModalBackdrop>
+          <ModalContent>
+            <img
+              src={Fechar}
+              alt="Fechar"
+              onClick={() => setIsModalOpen(false)}
+            />
+            <img src={Pizza} />
+            <div>
+              <h3>Pizza Marguerita</h3>
+              <p>
+                A pizza Margherita é uma pizza clássica da culinária italiana,
+                reconhecida por sua simplicidade e sabor inigualável. Ela é
+                feita com uma base de massa fina e crocante, coberta com molho
+                de tomate fresco, queijo mussarela de alta qualidade, manjericão
+                fresco e azeite de oliva extra-virgem. A combinação de sabores é
+                perfeita, com o molho de tomate suculento e ligeiramente ácido,
+                o queijo derretido e cremoso e as folhas de manjericão frescas,
+                que adicionam um toque de sabor herbáceo. É uma pizza simples,
+                mas deliciosa, que agrada a todos os paladares e é uma ótima
+                opção para qualquer ocasião.
+              </p>
+              <p>Serve: de 2 a 3 pessoas</p>
+              <AddButton>Adicionar ao carrinho - R$ 60,90</AddButton>
+            </div>
+          </ModalContent>
+        </ModalBackdrop>
+      )}
+    </div>
+  )
+}
 
 export default Perfil
--- src/components/PerfilRestaurante/styles.ts
+++ src/components/PerfilRestaurante/styles.ts
@@ -122,7 +122,7 @@ export const CardDescription = styled.p`
   line-height: 1.5;
 `
 
-export const AddButton = styled.button`
+export const AbrirModal = styled.button`
   width: 100%;
   height: 24px;
   background-color: ${cores.laranjaClaro};
@@ -134,7 +134,74 @@ export const AddButton = styled.button`
   margin-top: 8px;
 
   &:hover {
-    background-color: ${cores.laranjaEscuro};
+    background-color: ${cores.corFundo};
+    color: ${cores.laranjaEscuro};
+  }
+`
+
+export const ModalBackdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`
+
+export const ModalContent = styled.div`
+  background: ${cores.laranjaEscuro};
+  width: 1024px;
+  height: 344px;
+  position: absolute;
+  display: flex;
+  padding: 24px;
+
+  img:first-child {
+    cursor: pointer;
+    position: absolute;
+    top: 16px;
+    right: 16px;
+  }
+
+  img:nth-child(2) {
+    width: 280px;
+    height: 280px;
+    margin-right: 24px;
+    object-fit: fill;
+  }
+
+  h3 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 16px;
     color: ${cores.laranjaClaro};
+  }
+
+  p {
+    margin-bottom: 16px;
+    font-size: 14px;
+    line-height: 22px;
+    color: ${cores.laranjaClaro};
+  }
+`
+
+export const AddButton = styled.button`
+  width: 218px;
+  height: 24px;
+  background-color: ${cores.laranjaClaro};
+  color: ${cores.laranjaEscuro};
+  border: none;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 8px;
+
+  &:hover {
+    background-color: ${cores.corFundo};
+    color: ${cores.laranjaEscuro};
   }
 `
